3f3a5d27975faafe0b46bfc36299886e49eaff42
Alex Medearis <alexmedearis@gmail.com>
2012-04-30 15:21:50 -0500

    displayStatistics

---
+++ BViOSSDK/BViOSSDK/BVDisplayStatistics.h
@@ -0,0 +1,13 @@
+//
+//  BVDisplayStatistics.h
+//  BViOSSDK
+//
+//  Created by Alex Medearis on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BVBase.h"
+
+@interface BVDisplayStatistics : BVBase
+
+@end
---
+++ BViOSSDK/BViOSSDK/BVDisplayStatistics.m
@@ -0,0 +1,13 @@
+//
+//  BVDisplayStatistics.m
+//  BViOSSDK
+//
+//  Created by Alex Medearis on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BVDisplayStatistics.h"
+
+@implementation BVDisplayStatistics
+
+@end
