4b017709790d162395bb5f3073f29e230154894c
Alex Medearis <alexmedearis@gmail.com>
2012-05-04 12:48:16 -0500

    beginning commenting

--- BViOSSDK/BViOSSDK/BVBase.h
+++ BViOSSDK/BViOSSDK/BVBase.h
@@ -1,10 +1,18 @@
 //
-//  BVDisplay.h
+//  BVBase.h
 //  bazaarvoiceSDK
 //
 //  Created by Bazaarvoice Engineering on 2/21/12.
 //  Copyright (c) 2012 Bazaarvoice Inc. All rights reserved.
 //
+
+
+/*!
+ @header BVBase
+ BVBase is the base class for all API calls.  It should never be called directly. Instead, its subclasses should override the necessary parameters and functions to generate a specific API call. 
+ */
+
+
 
 #import <Foundation/Foundation.h>
 #import "BVSettings.h"
@@ -16,6 +24,10 @@
 
 @class BVBase;
 
+/*!
+ @protocol BVDelegate
+ This is the delegate protocol 
+ */
 @protocol BVDelegate <NSObject>
 @optional
 - (void) didReceiveResponse:(BVResponse*)response forRequest:(BVBase*)request;
@@ -27,6 +39,9 @@
     NSString *_rawURLRequest;
 }
 
+/*!
+ The client delegate which will receive BVDelegate notifications
+ */
 @property (nonatomic, unsafe_unretained) id<BVDelegate> delegate;
 @property (nonatomic, strong) BVParameters* parameters;
 @property (nonatomic, readonly) NSString* rawURLRequest;
@@ -40,8 +55,18 @@
 @property (nonatomic, readonly) NSString* displayType;
 - (NSString*) fragmentForKey:(NSString*)key usingDictionary:(NSDictionary*)parametersDict; // Override this function in subclass if want to build a fragment different from default
 - (NSString*) buildURLString; // Override for building URL requeststrings
-- (NSMutableURLRequest*) generateURLRequestWithString:(NSString*)string; // Override for generating the URL request.
 
-// Methods
+/*!
+ Override for generating the URL request. 
+ @param string
+ URL
+ @result
+ R in a URLRequest for the provided url.
+ */
+- (NSMutableURLRequest*) generateURLRequestWithString:(NSString*)string; // 
+
+/*!
+ This is the only method called by the instantiating class. It executes an asynchronous API request and will call one of 2 methods declared in the BVDelegate protocol.
+ */
 - (void) startAsynchRequest;
 @end
--- BViOSSDK/BViOSSDK/BVDisplayAnswer.h
+++ BViOSSDK/BViOSSDK/BVDisplayAnswer.h
@@ -6,6 +6,11 @@
 //  Copyright (c) 2012 Bazaarvoice Inc. All rights reserved.
 //
 
+/*!
+ @header BVDisplayAnswer
+ Request class for fetching answers.
+ */
+
 #import "BVBase.h"
 
 @interface BVDisplayAnswer : BVBase
