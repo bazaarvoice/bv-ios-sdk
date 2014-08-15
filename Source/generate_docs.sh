e8f65bfc338ca92f13840f8d4d8d1255dc9c5232
Jason Harris <jasonharris1991@gmail.com>
2014-08-15 15:21:26 -0500

    Fixed broken building and doc generation scripts

--- Source/generate_docs.sh
+++ Source/generate_docs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd  BVSDK/BVSDK
-appledoc --index-desc ../../Misc/desc.txt --project-name "Bazaarvoice iOS SDK" --no-repeat-first-par --project-company "Bazaarvoice" --company-id com.bazaarvoice --output . --no-install-docset .
+appledoc --index-desc ../../Misc/desc.txt --project-name "Bazaarvoice iOS SDK" --no-repeat-first-par --project-company "Bazaarvoice" --company-id com.bazaarvoice --keep-intermediate-files --no-install-docset .
 rm -rf ../../../Docs
 mv docset/Contents/Resources/Documents ../../../Docs
 rm -rf docset
+rm -rf html
