351eb8b0add1016759fccdde5f9bd4d3c68fc10a
Eric Miller <eric.miller@mutualmobile.com>
2015-09-22 13:55:40 -0500

    Converting deprecated NSURLConnection to NSURLSession

--- Pod/Classes/BVNetwork.h
+++ Pod/Classes/BVNetwork.h
@@ -12,7 +12,7 @@
 #define SDK_HEADER_NAME @"X-UA-BV-SDK"
 #define SDK_HEADER_VALUE @"IOS_SDK_V228"
 
-@interface BVNetwork : NSObject<NSURLConnectionDataDelegate>
+@interface BVNetwork : NSObject<NSURLSessionDataDelegate>
 
 - (id)initWithSender:(id)sender;
 
