fa09a673d0a8b7782239478d696b2c8847ddb5ae
Tim Kelly <tim.kelly@bazaarvoice.com>
2016-06-29 10:34:58 -0500

    Updates for 4.0.3 release, many stylistic updates and grammar fixes from tech writer team

--- deploy_android_qa.sh
+++
@@ -1,3 +0,0 @@
-jekyll clean --config _config_android.yml
-jekyll build --config _config_android.yml --verbose
-aws s3 cp _site_android/ s3://nexus-private-artifacts/utilitybelt/static_html/github-pages-android-sdk --recursive
--- deploy_ios_qa.sh
+++
@@ -1,3 +0,0 @@
-jekyll clean --config _config_ios.yml
-jekyll build --config _config_ios.yml --verbose
-aws s3 cp _site_ios/ s3://nexus-private-artifacts/utilitybelt/static_html/github-pages-ios-sdk --recursive
