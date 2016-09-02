492088c25725edd41cb441145c92b5297221177c
Sergio Azua <sergio.azua@bazaarvoice.com>
2016-09-02 15:56:22 -0500

    - Add deprecation flags for BVCurationsFeedRequest parameters: explicitPermission, perGroupLimit - Add in progressive zoom on Curations photo map and zooming to photos near store location

--- Examples/BVSDKDemo/BVSDKDemo/Curations/CurationsPhotoMapViewController.swift
+++ Examples/BVSDKDemo/BVSDKDemo/Curations/CurationsPhotoMapViewController.swift
@@ -14,104 +14,209 @@ import SDWebImage
 
 class CurationsPhotoMapViewController: UIViewController, MKMapViewDelegate {
     
+    let distanceThreshold = 120000.0
+    let minGroupZoom = 0.4
+    let storeZoom = 0.025
+    var hasShownDefault = false
     var curationsFeed : [BVCurationsFeedItem] = []
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var zoomOutBtn: UIButton!
+    @IBOutlet var btnImage: UIImageView!
     
-    init(curationsFeed : [BVCurationsFeedItem]){
-        self.curationsFeed = curationsFeed
-        super.init(nibName: nil, bundle: nil)
-        self.title = "Curations Photo Map"
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    var groupedFeedItems = [[BVCurationsFeedItem]]()
+    var previousRegion:  MKCoordinateRegion?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let mapView = MKMapView(frame: self.view.bounds)
-        mapView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
-        mapView.delegate = self
-        self.view.addSubview(mapView)
-        
-        // Annotations
-        mapView.addAnnotations(self.annotations())
+        self.title = "Curations Photo Map"
+        btnImage.image = btnImage.image?.imageWithRenderingMode(.AlwaysTemplate)
+        btnImage.tintColor = UIColor.redColor()
+        btnImage.backgroundColor = UIColor.whiteColor()
+        btnImage.layer.cornerRadius = btnImage.frame.width / 2
+        mapView.addAnnotations(self.annotations)
     }
     
-    func annotations() -> [JPSThumbnailAnnotation]{
+    override func viewDidAppear(animated: Bool) {
+        if !hasShownDefault {
+            hasShownDefault = true
+            guard let store = LocationPreferenceUtils.getDefaultStore()else{ return }
+            for ann in annotations {
+                if ann.coordinate.latitude == Double(store.latitude) {
+                    mapView.selectAnnotation(ann, animated: true)
+                    break
+                }
+            }
+        }
+    }
+    
+    lazy var annotations: [JPSThumbnailAnnotation] = {
+        [unowned self] in
         
         var annotationThumbs : [JPSThumbnailAnnotation] = []
         
-        var thumbId : UInt = 0
+        var idx : Int = 0
         for feedItem in self.curationsFeed {
+            let location = feedItem.coordinates
             
-            let location : BVCurationsCoordinates = feedItem.coordinates
-            
-            if location.longitude != nil && location.latitude != nil {
-                
-                let currThumb = JPSThumbnail()
-            
-                if feedItem.photos != nil && feedItem.photos.count > 0 {
-                     currThumb.imageURL = NSURL(string: feedItem.photos[0].imageServiceUrl)
-                } else {
+            if let _ = location.longitude, _ = location.latitude, _ = feedItem.photos{
+                if feedItem.photos.count == 0 {
                     continue; // skip anything that doesn't have any image
                 }
-        
-                currThumb.title = feedItem.author.username
-                currThumb.subtitle = feedItem.channel
-                currThumb.coordinate = CLLocationCoordinate2DMake(location.latitude.doubleValue, location.longitude.doubleValue)
-                currThumb.thumbId = thumbId
                 
-                currThumb.disclosureBlock = {
-                    
+                self.groupFeedItem(feedItem)
+                
+                let i = idx;
+                let annotation = JPSThumbnailAnnotation(curationsFeedItem: feedItem) {
                     // navigate to the curations lightbox from the user-selected image from the map annotation
-                    
                     let targetVC = CurationsFeedMasterViewController()
-                    
-                    let titleLabel = UILabel(frame: CGRectMake(0,0,200,44))
-                    titleLabel.text = "Social Feed";
-                    titleLabel.textColor = UIColor.whiteColor()
-                    titleLabel.textAlignment = .Center
-                    targetVC.navigationItem.titleView = titleLabel
-                    
                     targetVC.socialFeedItems = self.curationsFeed
-                    targetVC.startIndex = Int(currThumb.thumbId)
+                    targetVC.startIndex = i
                     self.navigationController?.pushViewController(targetVC, animated: true)
-                    
                 }
                 
-                annotationThumbs.append(JPSThumbnailAnnotation(thumbnail: currThumb))
-                thumbId = thumbId + 1
+                annotationThumbs.append(annotation)
+                idx += 1
             }
-            
         }
         
         return annotationThumbs
-    }
-        
-    // MARK: MKMapViewDelegate
+    }()
     
-    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
-        
-        if view.conformsToProtocol(JPSThumbnailAnnotationViewProtocol) {
-            (view as! JPSThumbnailAnnotationViewProtocol).didSelectAnnotationViewInMap(mapView)
+    private func groupFeedItem(feedItem: BVCurationsFeedItem){
+        var itemGroup : [BVCurationsFeedItem]?
+        for group in groupedFeedItems {
+            for item in group {
+                if getDistanceBetween(feedItem, feedItem2: item) < distanceThreshold {
+                    itemGroup = group
+                    let idx = groupedFeedItems.indexOf({$0 == itemGroup!})
+                    groupedFeedItems.removeAtIndex(idx!)
+                    break
+                }
+            }
+            if let _ = itemGroup {
+                break
+            }
         }
         
+        if itemGroup == nil {
+            itemGroup = [BVCurationsFeedItem]()
+        }
+        
+        itemGroup?.append(feedItem)
+        groupedFeedItems.append(itemGroup!)
+    }
+    
+    private func getDistanceBetween(feedItem1: BVCurationsFeedItem, feedItem2: BVCurationsFeedItem) -> Double {
+        let loc1 = CLLocation(latitude: feedItem1.coordinates.latitude.doubleValue, longitude: feedItem1.coordinates.longitude.doubleValue)
+        let loc2 = CLLocation(latitude: feedItem2.coordinates.latitude.doubleValue, longitude: feedItem2.coordinates.longitude.doubleValue)
+        
+        return loc1.distanceFromLocation(loc2)
+    }
+    
+    // MARK: MKMapViewDelegate
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        if !animated {
+            shouldHideZoomout(false)
+        }
+    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        shouldHideZoomout(false)
+        if let jpsView = view as? JPSThumbnailAnnotationView, annotation = view.annotation as? JPSThumbnailAnnotation {
+            let feedItem = annotation.curationsFeedItem
+            
+            if mapView.region.span.latitudeDelta < 2.0 {
+                jpsView.didSelectAnnotationViewInMap(mapView)
+                let loc = feedItem.coordinates;
+                let region = MKCoordinateRegionMake(CLLocationCoordinate2D(latitude: loc.latitude!.doubleValue, longitude: loc.longitude!.doubleValue), MKCoordinateSpanMake(storeZoom, storeZoom))
+                mapView.setRegion(region, animated: true)
+            }else {
+                mapView.deselectAnnotation(annotation, animated: false)
+                var itemGroup: [BVCurationsFeedItem]?
+                for group in groupedFeedItems {
+                    for item in group {
+                        if item === feedItem {
+                            itemGroup = group
+                            break
+                        }
+                    }
+                    if let _ = itemGroup {
+                        break
+                    }
+                }
+                
+                var minLong = 0.0
+                var maxLong = -180.0
+                var minLat = 90.0
+                var maxLat = 0.0
+                
+                for item in itemGroup! {
+                    if item.coordinates.longitude.doubleValue < minLong {
+                        minLong = item.coordinates.longitude.doubleValue
+                    }
+                    
+                    if item.coordinates.longitude.doubleValue > maxLong {
+                        maxLong = item.coordinates.longitude.doubleValue
+                    }
+                    
+                    if item.coordinates.latitude.doubleValue < minLat {
+                        minLat = item.coordinates.latitude.doubleValue
+                    }
+                    
+                    if item.coordinates.latitude.doubleValue > maxLat {
+                        maxLat = item.coordinates.latitude.doubleValue
+                    }
+                }
+                //Getting center
+                let avgLat = (minLat + maxLat) / 2.0
+                let avgLong = (minLong + maxLong) / 2.0
+                //getting viewport + add padding
+                var spanLong = abs(maxLong - minLong) + 0.3
+                spanLong = spanLong > minGroupZoom ? spanLong : minGroupZoom
+                var spanLat = abs(maxLat - minLat) + 0.3
+                spanLat = spanLat > minGroupZoom ? spanLat : minGroupZoom
+
+                let region = MKCoordinateRegionMake(CLLocationCoordinate2D(latitude: avgLat, longitude: avgLong), MKCoordinateSpanMake(spanLat, spanLong))
+                mapView.setRegion(region, animated: true)
+                previousRegion = region
+            }
+        }
     }
     
     func mapView(mapView: MKMapView, didDeselectAnnotationView view: MKAnnotationView) {
-        if view.conformsToProtocol(JPSThumbnailAnnotationViewProtocol) {
-            (view as! JPSThumbnailAnnotationViewProtocol).didDeselectAnnotationViewInMap(mapView)
+        if let jpsView = view as? JPSThumbnailAnnotationView {
+                jpsView.didDeselectAnnotationViewInMap(mapView)
         }
     }
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
         
-        if annotation.conformsToProtocol(JPSThumbnailAnnotationProtocol) {
-            return (annotation as! JPSThumbnailAnnotationProtocol).annotationViewInMap(mapView)
+        if let jpsAnnotation = annotation as? JPSThumbnailAnnotation {
+            return jpsAnnotation.annotationViewInMap(mapView)
         }
-            return nil
+        return nil
         
     }
     
+    @IBAction internal func resetZoomAnimated(sender: UIButton?) {
+        resetZoom(true)
+    }
+    
+    private func resetZoom(animated: Bool) {
+        
+        if previousRegion != nil && mapView.region.span.latitudeDelta < minGroupZoom  {
+            mapView.setRegion(previousRegion!, animated: animated)
+        }else{
+            previousRegion = nil
+            let coord = CLLocationCoordinate2D(latitude: 39.50, longitude: -98.35)
+            let region = MKCoordinateRegionMake(coord, MKCoordinateSpanMake(59.0, 59.0))
+            mapView.setRegion(region, animated: animated)
+            shouldHideZoomout(true)
+        }
+    }
+    
+    private func shouldHideZoomout(shouldHide:Bool) {
+        zoomOutBtn.hidden = shouldHide
+        btnImage.hidden = shouldHide
+    }
 }
---
+++ Examples/BVSDKDemo/BVSDKDemo/Curations/JPSThumbnailAnnotation+Curations.swift
@@ -0,0 +1,37 @@
+//
+//  JPSThumbnailAnnotation+Curations.swift
+//  BVSDKDemo
+//
+//  Copyright © 2016 Bazaarvoice. All rights reserved.
+//
+
+import UIKit
+import BVSDK
+import ObjectiveC
+
+private var feedItemAssociationKey: UInt8 = 0
+
+extension JPSThumbnailAnnotation {
+
+    convenience init(curationsFeedItem: BVCurationsFeedItem, action: () -> Void ) {
+        let thumbNail = JPSThumbnail()
+
+        thumbNail.title = curationsFeedItem.author.username
+        thumbNail.subtitle = curationsFeedItem.channel
+        thumbNail.imageURL = NSURL(string: curationsFeedItem.photos[0].imageServiceUrl)
+        thumbNail.coordinate = CLLocationCoordinate2DMake(curationsFeedItem.coordinates.latitude.doubleValue, curationsFeedItem.coordinates.longitude.doubleValue)
+        
+        thumbNail.disclosureBlock = {
+            action()
+        }
+
+        self.init(thumbnail: thumbNail)
+        objc_setAssociatedObject(self, &feedItemAssociationKey, curationsFeedItem, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
+    
+    var curationsFeedItem: BVCurationsFeedItem! {
+        get {
+            return objc_getAssociatedObject(self, &feedItemAssociationKey) as! BVCurationsFeedItem
+        }
+    }
+}
