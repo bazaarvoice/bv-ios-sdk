b110f437f3f400b11f9bf43e8b41adb5446f5815
Tim Kelly <tim.kelly@bazaarvoice.com>
2016-11-01 12:50:39 -0500

    Add missing CoreGraphics include for manual install.

--- Pod/BVCommon/BVCore.h
+++ Pod/BVCommon/BVCore.h
@@ -18,11 +18,11 @@
 
 /// Provides the master version of the SDK.
 
-#define BV_SDK_VERSION @"5.0.0"
+#define BV_SDK_VERSION @"5.0.1"
 
 /// Conversation SDK Version
 #define SDK_HEADER_NAME @"X-UA-BV-SDK"
-#define SDK_HEADER_VALUE @"IOS_SDK_V500"
+#define SDK_HEADER_VALUE @"IOS_SDK_V501"
 
 /// Error domain for NSError results, when present.
 #define BVErrDomain @"com.bvsdk.bazaarvoice"
