b078498f1cc8ca554b61eb493f9eeb844351d927
Michael Van Milligan <michael.vanmilligan@bazaarvoice.com>
2018-07-30 15:42:53 -0500

    MOB-864: Update SDK to latest Recommendations Analytics.

--- BVSDK/BVCommon/BVSDKConstants.h
+++ BVSDK/BVCommon/BVSDKConstants.h
@@ -11,11 +11,11 @@
 
 /// Provides the master version of the SDK.
 
-#define BV_SDK_VERSION @"7.0.1"
+#define BV_SDK_VERSION @"7.0.2"
 
 /// Conversation SDK Version
 #define SDK_HEADER_NAME @"X-UA-BV-SDK"
-#define SDK_HEADER_VALUE @"IOS_SDK_V701"
+#define SDK_HEADER_VALUE @"IOS_SDK_V702"
 
 /// Error domain for NSError results, when present.
 #define BVErrDomain @"com.bazaarvoice.bvsdk"
