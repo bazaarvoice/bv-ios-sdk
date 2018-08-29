098412b912f7339655da1cd68cc11a512d84fd63
Michael Van Milligan <michael.vanmilligan@bazaarvoice.com>
2018-08-29 10:55:03 -0500

    Bump 7.1.0 release version

--- BVSDK/BVCommon/BVSDKConstants.h
+++ BVSDK/BVCommon/BVSDKConstants.h
@@ -11,11 +11,11 @@
 
 /// Provides the master version of the SDK.
 
-#define BV_SDK_VERSION @"7.0.2"
+#define BV_SDK_VERSION @"7.1.0"
 
 /// Conversation SDK Version
 #define SDK_HEADER_NAME @"X-UA-BV-SDK"
-#define SDK_HEADER_VALUE @"IOS_SDK_V702"
+#define SDK_HEADER_VALUE @"IOS_SDK_V710"
 
 /// Error domain for NSError results, when present.
 #define BVErrDomain @"com.bazaarvoice.bvsdk"
