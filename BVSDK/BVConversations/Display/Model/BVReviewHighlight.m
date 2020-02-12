eb231e1d3851c171a65d17c740272bcda1fd30ab
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-02-12 12:32:09 +0530

    Added Request, Response and Model classes for ReviewHighlights.

---
+++ BVSDK/BVConversations/Display/Model/BVReviewHighlight.m
@@ -0,0 +1,12 @@
+//
+//  BVReviewHighlight.m
+//  BVSDK
+//
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+// 
+
+#import "BVReviewHighlight.h"
+
+@implementation BVReviewHighlight
+
+@end
---
+++ BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsResponse.h
@@ -0,0 +1,16 @@
+//
+//  BVReviewHighlightsResponse.h
+//  BVSDK
+//
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+// 
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BVReviewHighlightsResponse : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
