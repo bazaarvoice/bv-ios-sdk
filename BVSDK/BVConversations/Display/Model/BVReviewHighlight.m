429b72ff824f96ffcfbe1e93eedf6e09449e02a0
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-02-19 16:24:55 +0530

    Added JSON Mapping logic.

--- BVSDK/BVConversations/Display/Model/BVReviewHighlight.m
+++ BVSDK/BVConversations/Display/Model/BVReviewHighlight.m
@@ -11,6 +11,46 @@
 
 @implementation BVReviewHighlight
 
+- (id)initWithApiResponse:(id)apiResponse {
+    return self;
+}
 
+- (id)initWithTitle:(NSString *)title content:(id)content {
+    
+    if ((self = [super init])) {
+        
+        self.title = title;
+        
+        if (!content || ![content isKindOfClass:[NSDictionary class]]) {
+            return nil;
+        }
+        
+        NSDictionary *apiObject = (NSDictionary *)content;
+        
+        SET_IF_NOT_NULL(self.mentionsCount, apiObject[@"mentionsCount"])
+        SET_IF_NOT_NULL(self.presenceCount, apiObject[@"presenceCount"])
+        
+        NSArray<NSDictionary *> *bestExamplesDictionary = (NSArray<NSDictionary *> *)[apiObject objectForKey:@"bestExamples"];
+        
+        NSMutableArray *bestExamplesArrayBuilder = [NSMutableArray array];
+        
+        for (NSDictionary *bestExample in bestExamplesDictionary) {
+            BVReviewHighligtsReview *review = [[BVReviewHighligtsReview alloc] initWithApiResponse:bestExample];
+            
+            [bestExamplesArrayBuilder addObject:review];
+        }
+        
+        if (bestExamplesArrayBuilder.count == 0) {
+            return nil;
+        }
+        
+        self.bestExamples = bestExamplesArrayBuilder;
+        
+
+    }
+    
+    
+    return self;
+}
 
 @end
