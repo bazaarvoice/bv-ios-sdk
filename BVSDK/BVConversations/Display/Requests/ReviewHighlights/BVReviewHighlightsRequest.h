79e1c8478bcbe7c56c72e74ae4eb074c016416b7
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-02-24 11:17:27 +0530

    Added properties and methods to BVReviewHighlightsRequest.

--- BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsRequest.h
+++ BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsRequest.h
@@ -5,8 +5,18 @@
 //  Copyright © 2020 Bazaarvoice. All rights reserved.
 // 
 
-#import <Foundation/Foundation.h>
+#import "BVReviewHighlightsResponse.h"
+#import "BVConversationsRequest.h"
 
-@interface BVReviewHighlightsRequest : NSObject
+@interface BVReviewHighlightsRequest : BVConversationsRequest
+
+@property(nonnull, readonly) NSString *productId;
+
+- (nonnull instancetype)initWithProductId:(nonnull NSString *)productId;
+
+- (nonnull instancetype)__unavailable init;
+
+- (void)load:(nonnull void (^)(BVReviewHighlightsResponse *__nonnull response))success
+failure:(nonnull ConversationsFailureHandler)failure;
 
 @end
