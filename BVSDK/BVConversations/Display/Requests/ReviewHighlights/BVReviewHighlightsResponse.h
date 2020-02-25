0371de4d4eba35655c5763f951502e0579d1f2d0
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-02-25 11:55:47 +0530

    Response Mapping.

--- BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsResponse.h
+++ BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsResponse.h
@@ -6,10 +6,13 @@
 // 
 
 #import <Foundation/Foundation.h>
+#import "BVReviewHighlights.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BVReviewHighlightsResponse : NSObject
+
+@property(nonnull) BVReviewHighlights *reviewHighlights;
 
 - (nonnull instancetype)initWithApiResponse:(nonnull NSDictionary *)apiResponse;
 
