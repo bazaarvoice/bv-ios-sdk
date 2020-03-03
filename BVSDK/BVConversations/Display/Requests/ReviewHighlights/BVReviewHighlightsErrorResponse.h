c9e3e100792aaf68e693618286e6eed3701349d2
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-03-03 11:32:23 +0530

    Added BVReviewHighlightsErrorResponse file.

---
+++ BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsErrorResponse.h
@@ -0,0 +1,16 @@
+//
+//  BVReviewHighlightsErrorResponse.h
+//  BVSDK
+//
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+// 
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BVReviewHighlightsErrorResponse : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
