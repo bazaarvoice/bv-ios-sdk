0451e5a7604b64a6d05871bbe6c76d8da61dcef4
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-03-04 13:06:10 +0530

    Added comments.

--- BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsRequest.h
+++ BVSDK/BVConversations/Display/Requests/ReviewHighlights/BVReviewHighlightsRequest.h
@@ -8,15 +8,21 @@
 #import "BVReviewHighlightsResponse.h"
 #import "BVConversationsRequest.h"
 
+/** This class allow you to build request parameters and request a user profile
+ * (reviewHighlight) and accepted content (productId)
+ */
 @interface BVReviewHighlightsRequest : BVConversationsRequest
 
 @property(nonnull, readonly) NSString *productId;
 
+/// The details (reviewHighlights) of the product you are trying to fetch
 - (nonnull instancetype)initWithProductId:(nonnull NSString *)productId;
 
 - (nonnull instancetype)__unavailable init;
 
+/// Make an asynch http request to fetch the Author's profile data.
+/// BVReviewHighlightsResponse model for available fields.
 - (void)load:(nonnull void (^)(BVReviewHighlightsResponse *__nonnull response))success
-failure:(nonnull ConversationsFailureHandler)failure;
+     failure:(nonnull ConversationsFailureHandler)failure;
 
 @end
