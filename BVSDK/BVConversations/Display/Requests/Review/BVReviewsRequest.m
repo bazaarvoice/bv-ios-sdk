9c1b6fb9470bd3bc24a23d775408475d6b983c79
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-08-31 18:01:51 +0530

    Code refactoring - moved incentivizedStats property to BVReviewsRequest from BVBaseReviewsRequest.

--- BVSDK/BVConversations/Display/Requests/Review/BVReviewsRequest.m
+++ BVSDK/BVConversations/Display/Requests/Review/BVReviewsRequest.m
@@ -16,6 +16,7 @@
 - (nonnull instancetype)initWithProductId:(nonnull NSString *)productId
                                     limit:(NSUInteger)limit
                                    offset:(NSUInteger)offset {
+    self.incentivizedStats = NO;
     return self = [super initWithID:productId limit:limit offset:offset primaryFilter: BVReviewFilterValueProductId];
 }
 
@@ -23,7 +24,18 @@
                          andFilter:(BVReviewFilterValue)filter
                              limit:(NSUInteger)limit
                             offset:(NSUInteger)offset {
+    self.incentivizedStats = NO;
     return self = [super initWithID:Id limit:limit offset:offset primaryFilter: filter];
+}
+
+- (nonnull NSMutableArray *)createParams {
+  NSMutableArray<BVStringKeyValuePair *> *params = [super createParams];
+  
+  if (self.incentivizedStats == YES) {
+    [params addObject:[BVStringKeyValuePair pairWithKey:@"incentivizedstats" value:@"true"]];
+  }
+  
+  return params;
 }
 
 - (NSString *)productId {
