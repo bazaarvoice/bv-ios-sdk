8050042fd516ee39cd1a8bc558fca6ef817a7a98
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-09-10 12:39:33 +0530

    Created BulkProductTests class and added method stubs.

---
+++ BVSDKTests/ConversationsTests/DisplayTests/BulkProductTests.swift
@@ -0,0 +1,41 @@
+//
+//  BulkProductTests.swift
+//  BVSDK
+//
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+// 
+
+import XCTest
+
+class BulkProductTests: XCTestCase {
+  
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    let configDict = ["clientId": "apitestcustomer",
+                      "apiKeyConversations": "KEY_REMOVED"];
+    BVSDKManager.configure(withConfiguration: configDict, configType: .staging)
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testBulkProductIncentivizedStats() {
+    let expectation = self.expectation(description: "testBulkProductIncentivizedStats")
+    
+    let request = BVBulkProductRequest()
+    request.load({ (response) in
+      
+      print(response.results.count)
+      
+      expectation.fulfill()
+    }) { (error) in
+       XCTFail("product display request error: \(error)")
+       expectation.fulfill()
+    }
+    
+    self.waitForExpectations(timeout: 10000) { (error) in
+      XCTAssertNil(error, "Something went horribly wrong, request took too long.")
+    }
+  }
+}
