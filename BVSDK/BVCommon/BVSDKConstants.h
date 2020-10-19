f63c8da982705b2721733c677959577ca0858deb
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-10-19 11:09:28 +0530

    Release 8.3.0

--- BVSDK/BVCommon/BVSDKConstants.h
+++ BVSDK/BVCommon/BVSDKConstants.h
@@ -10,11 +10,11 @@
 #define BVSDKConstants_h
 
 /// Provides the master version of the SDK.
-#define BV_SDK_VERSION @"8.2.4"
+#define BV_SDK_VERSION @"8.3.0"
 
 /// Conversation SDK Version
 #define SDK_HEADER_NAME @"X-UA-BV-SDK"
-#define SDK_HEADER_VALUE @"IOS_SDK_V824"
+#define SDK_HEADER_VALUE @"IOS_SDK_V830"
 
 /// Error domain for NSError results, when present.
 #define BVErrDomain @"com.bazaarvoice.bvsdk"
