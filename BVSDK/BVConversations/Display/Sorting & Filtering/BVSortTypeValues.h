fd889b310cd032f8ef72d10d60d6eac2cea5d895
Abishekh S Kamath <abishekhs.kamath@bazaarvoice.com>
2022-07-25 16:13:31 +0530

    Add Sort by relevancy

---
+++ BVSDK/BVConversations/Display/Sorting & Filtering/BVSortTypeValues.h
@@ -0,0 +1,13 @@
+//
+//  BVSortTypeValues.h
+//  BVSDK
+//
+//  Copyright © 2022 Bazaarvoice. All rights reserved.
+// 
+
+#ifndef BVSortTypeValues_h
+#define BVSortTypeValues_h
+
+#import "BVRelevancySortTypeValue.h"
+
+#endif /* BVSortTypeValues_h */
