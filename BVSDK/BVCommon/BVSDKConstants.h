4228a2649fb0922c455abb01efb525fa22ff71ad
rahulbawane <rahul.bawane@bazaarvoice.com>
2025-03-05 14:37:57 +0530

    :bookmark: Release 8.13.3

--- BVSDK/BVCommon/BVSDKConstants.h
+++ BVSDK/BVCommon/BVSDKConstants.h
@@ -10,7 +10,7 @@
 #define BVSDKConstants_h
 
 /// Provides the master version of the SDK.
-#define BV_SDK_VERSION @"8.13.2"
+#define BV_SDK_VERSION @"8.13.3"
 
 /// Conversation SDK Version
 #define SDK_HEADER_NAME @"X-UA-BV-SDK"
